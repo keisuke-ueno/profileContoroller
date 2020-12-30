8c806bb947a2921bc6237b3f77944c195c6209f1
keisuke.ueno <eesema@icloud.com>
2020-12-30 05:16:59 +0000

    my-news

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,11 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>uenor</title>
     </head>
     <body>
         <h1>Myニュース作成画面</h1>
         <h2>私は、上野圭介です</h2>
-        <h3>Ueno Coffee factoryというコーヒービーンズショップをやっています</h3>
+        <h3>コーヒービーンズショップをやってい</h3>
     </body>
 </html>
