e486232a6b0bec9751fc4a0b07cabf1d06f21a0b
keisuke.ueno <eesema@icloud.com>
2020-12-30 04:34:21 +0000

    ny news

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,16 @@
 {{-- layyouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
 @extends('layouts.profile')
+
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成’を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'プロフ')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
-            <h2>ニュース新規作成</h2>
+            <h2>プロフィールの新規作成２回目</h2>
+            {{ 1+1 }}
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
-Route::group(['prefix '=> 'admin'], function() {
+Route::group(['prefix'=> 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
 });
-Route::group(['prefix '=> 'admin'], function() {
+Route::group(['prefix'=> 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
