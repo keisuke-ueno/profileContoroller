dd5e097b59301761115ff2c9d72315e8c3a35524
keisuke.ueno <eesema@icloud.com>
2021-01-02 09:06:28 +0000

    mynews

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -2,15 +2,15 @@
 @extends('layouts.profile')
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成’を埋め込む --}}
-@section('title', 'プロフ')
+@section('title', 'Laravel')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
-            <h2>プロフィールの新規作成２回目</h2>
-            {{ 1+1 }}
+            <h2>My プロフィール</h2>
+            
         </div>
     </div>
 </div>
