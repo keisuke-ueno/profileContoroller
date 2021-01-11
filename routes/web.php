82518e970a3f9086610f9b93fc91d24c961ed5fd
keisuke.ueno <eesema@icloud.com>
2021-01-11 03:45:28 +0000

    my news

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+ return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');# 追記
+     Route::post('profile/create','Admin\ProfileController@create')->middleware('auth');
+     Route::post('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
-Route::group(['prefix'=> 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-});
-Route::group(['prefix'=> 'admin'], function() {
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
-});
+
+
